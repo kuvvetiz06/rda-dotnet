8ccfe7ca2ff3926770b7abb62f84a62d42cf63b1
huseyin.gokdemir <huseyin.gokdemir@ronesans.com>
2025-12-02 14:26:37 +0300

    .

--- MyApp.Application/Documents/Commands/ExtractFieldsFromPdf/ExtractFieldsFromPdfCommand.cs
+++
@@ -1,8 +0,0 @@
-// PATH: MyApp.Application/Documents/Commands/ExtractFieldsFromPdf/ExtractFieldsFromPdfCommand.cs
-using System.IO;
-using MediatR;
-using MyApp.Domain.Documents;
-
-namespace MyApp.Application.Documents.Commands.ExtractFieldsFromPdf;
-
-public record ExtractFieldsFromPdfCommand(Stream PdfStream, string FileName, string Prompt) : IRequest<DocumentExtractionResult>;
